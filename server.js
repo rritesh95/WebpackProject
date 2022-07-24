0bf365df0c047bf03c8ca6fc0070ee9cc53481cb
Ritesh Patel <rritesh95@gmail.com>
2022-07-24 14:40:14 +0530

    webpack middleware in production

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
 const express = require('express');
-
-const webpackMiddleware = require('webpack-dev-middleware');
-const webpack = require('webpack');
-const webpackConfig = require('./webpack.config');
-//we require all three dependencies to make middleware work
+const path = require('path');
 
 const app = new express();
 
-app.use(webpackMiddleware(webpack(webpackConfig)));
+console.log(process.env.NODE_ENV);
+if(process.env.NODE_ENV !== 'production'){
+    const webpackMiddleware = require('webpack-dev-middleware');
+    const webpack = require('webpack');
+    const webpackConfig = require('./webpack.config');
+    //we require all three dependencies to make middleware work
+    app.use(webpackMiddleware(webpack(webpackConfig)));
+}else {
+    app.use(express.static('dist'));
+    app.get('*', (req,res) => {
+        res.sendFile(path.resolve(__dirname, 'dist/index.html'));
+    })
+}
 
-app.listen(3050, () => console.log('Listening'));
+app.listen(process.env.PORT || 3050, () => console.log('Listening'));
